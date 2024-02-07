c08e31c5e624199af119de45fd3e945b31ea97f5
araf95 <you@example.com>
2024-02-07 16:41:34 +0600

    Hero-added with css

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>New</p>
+                    <img src="{}" alt="" />
+                </div>
+                <p>make travel</p>
+                <p>comfortable</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Products</div>
+                <img src="" alt="" />
+            </div>
+
+        </div>
+        <div className="hero-right">
+            <img src="" alt="" />
+
+        </div>
+    </div>
+  )
+}
