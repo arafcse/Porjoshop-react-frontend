c209a486712da017730719d46dcc987e9033d040
araf95 <you@example.com>
2024-02-08 15:42:20 +0600

    Items and Popular Item section added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Hero.css'
+import arrowIcon from '../Assests/arrow_icon.png'
+import handIcon from '../Assests/hand_icon.png'
+import heroImage from '../Assests/hero_image.png'
 
 export const Hero = () => {
   return (
@@ -8,20 +11,20 @@ export const Hero = () => {
             <h2>NEW ARRIVALS ONLY</h2>
             <div>
                 <div className="hero-hand-icon">
-                    <p>New</p>
-                    <img src="{}" alt="" />
+                    <p>make</p>
+                    <img src={handIcon} alt="" />
                 </div>
-                <p>make travel</p>
+                <p>your travel</p>
                 <p>comfortable</p>
             </div>
             <div className="hero-latest-btn">
                 <div>Latest Products</div>
-                <img src="" alt="" />
+                <img src={arrowIcon} alt="" />
             </div>
 
         </div>
         <div className="hero-right">
-            <img src="" alt="" />
+            <img src={heroImage} alt="" />
 
         </div>
     </div>
