09a54d874fa1010aa7c488620946b32c08c85391
araf95 <you@example.com>
2024-02-13 22:03:56 +0600

    Product category & login page setup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {Product} from './Pages/Product';
 import {Cart} from './Pages/Cart';
 import {LoginSignup} from './Pages/LoginSignup';
 import { Footer } from './Components/Footer/Footer';
+import bag_banner from './Components/Assests/banner_mens.png';
+import cloth_banner from './Components/Assests/banner_kids.png';
+import tent_banner from './Components/Assests/banner_women.png';
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop />}/>
-        <Route path='/bags' element={<ShopCategory category="bags"/>}/>
-        <Route path='/cloths' element={<ShopCategory category="cloths"/>}/>
-        <Route path='/tents' element={<ShopCategory category="tents"/>}/>
+        <Route path='/bags' element={<ShopCategory banner={bag_banner} category="men"/>}/>
+        <Route path='/cloths' element={<ShopCategory banner={cloth_banner} category="women"/>}/>
+        <Route path='/tents' element={<ShopCategory banner={tent_banner} category="kid"/>}/>
         <Route path='/others' element={<ShopCategory category="others"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
